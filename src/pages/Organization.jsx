cae84b5c1c9163cf729eb7e9249893a251bf5713
FranckTshiz27 <francktshizbu27@gmail.com>
2023-01-05 21:45:12 +0100

    feat : add roboto font

--- src/pages/Organization.jsx
+++ src/pages/Organization.jsx
@@ -1,9 +1,10 @@
+import '../style/organization.scss';
 const Organization =()=>{
 
     return(
-        <>
-        <h1>MON ORGANISATION</h1>
-        </>
+        <div className='organization'>
+           <h2>40 organisations</h2>
+        </div>
     )
 
 }
